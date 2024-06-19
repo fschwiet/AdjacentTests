d9308297579babd94711bdd201180a9e88fdd12a
fschwiet <fschwiet@gmail.com>
2024-06-19 16:44:58 -0300

    add example class and adjacent test

---
+++ Library/RFC3092/Foo.cs
@@ -0,0 +1,6 @@
+﻿namespace Library.RFC3092;
+
+public class Foo
+{
+	public int Multiply(int a, int b) => a * b;
+}
---
+++ Library/RFC3092/FooTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.RFC3092;
+
+public class FooTests
+{
+	[Fact]
+	public void GetSame_works()
+	{
+		Assert.Equal(246, new Foo().Multiply(123, 2));
+	}
+}
